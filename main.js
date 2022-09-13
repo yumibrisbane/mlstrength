9abcd0d731293d2f993a298cb131386ca8e545cf
yumibrisbane <98505798+yumibrisbane@users.noreply.github.com>
2022-09-13 10:27:24 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-const burger = document.querySelector(".burger");
-const nav = document.querySelector(".nav-links");
-const navLinks = document.querySelectorAll(".nav-links li");
+function myFunction() {
+  var x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 
-burger.addEventListener("click", () => {
-  nav.classList.toggle("nav-active");
-
-  navLinks.forEach((link, index) => {
-    if(link.style.animation){
-      link.style.animation = "";
-    } else {
-      link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 0.4}s`;
-    }
-  });
-});
 function on() {
   document.getElementById("overlay").style.display = "block"; 
 }
